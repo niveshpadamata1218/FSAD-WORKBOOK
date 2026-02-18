783258e584c24cd7865243158900ea07d7e572a2
niveshpadamata1218 <2400030538@kluniversity.in>
2026-02-18 09:42:55 +0530

    Removed CRUD

--- CRUD-OP/src/main/java/com/klu/demo/repo/Studentrepo.java
+++
@@ -1,8 +0,0 @@
-package com.klu.demo.repo;
-import com.klu.demo.model.*;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Studentrepo extends JpaRepository<Student,Integer> {
-
-}
